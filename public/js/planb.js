30eb2f596fa235aaf410ef8663bd8348befcef4f
NathanHassanzadeh <ehassanz@ucsd.edu>
2018-03-13 15:39:15 -0700

    modified help text

--- public/js/planb.js
+++ public/js/planb.js
@@ -4,13 +4,11 @@ $(document).ready(function () {
   initializePage();
 });
 
-$("button").click(function() {
-	console.log("PageB");
-	ga('send', 'event', 'Page Click', 'On B');
-});
 
 function initializePage() {
   $('.plan').click(function (e) {
+    ga('send', 'event', 'Page Click', 'On B');
+    
     let planid  = $(this).attr('id');
     let groupid = $(this).closest('.grpplans').attr('id');
 
